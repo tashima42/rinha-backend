d31ed16b6d8dce32c89261be993df1c2c268c821
Pedro Tashima <pedro.tashima@suse.com>
2024-02-16 21:39:16 -0300

    create tranctstion handler

--- handlers.go
+++ handlers.go
@@ -1,7 +1,14 @@
 package main
 
+import "github.com/gofiber/fiber/v2"
+
 type Customer struct {
 	ID      int `bson:"id"`
 	Limit   int `bson:"limit"`
 	Balance int `bson:"balance"`
+}
+
+func handleCreateTransaction(c *fiber.Ctx) error {
+
+	return c.Status(200).JSON(map[string]interface{}{})
 }
